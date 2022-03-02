136657244d647601c5a2ee3d15795912b712c126
Sergio Elisondo <sergioelisondo@gmail.com>
2022-03-02 14:24:39 -0800

    React app tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Game from './components/Game';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div
+      style={{
+        backgroundImage: 'url("https://preview.redd.it/7jsccbavgs671.gif?width=1000&auto=webp&s=624eda20d8507c284555a62433bb3cec489e4acb")',
+        backgroundAttachment: 'fixed',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        width: '100vw',
+        height: '100vh'
+      }}
+    >
+       <Game />
     </div>
-  );
+  )
 }
 
+// diff backgrounds
+// https://preview.redd.it/7jsccbavgs671.gif?width=1000&auto=webp&s=624eda20d8507c284555a62433bb3cec489e4acb   city shot
+// https://cdnb.artstation.com/p/assets/images/images/026/128/253/original/lennart-butz-idea5anim5.gif?1587962321 japanese lake view
+
+
 export default App;
+
+// first, remove EVERYTHING in return method
